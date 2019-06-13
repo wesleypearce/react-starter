cf3717684bd81a8e2ce338b488ac76fbe406bb76
Wes Pearce <wespearce1@gmail.com>
2019-06-13 09:21:39 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'index.scss'
+
+let HelloWorld = () => {
+  return <h1>Hello World!</h1>
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'))
